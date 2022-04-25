972f5b60dce2b05feb61211400df179489a49a71
dhaval-galitein <dhaval@galitein.in>
2022-04-25 18:41:17 +0530

    Update TabContainer.js

--- src/renderer/container/TabContainer.js
+++ src/renderer/container/TabContainer.js
@@ -27,61 +27,38 @@ const TabContainer = ({ children }) => {
     setTabs([...tempTabs]);
   };
 
-  if (true) {
-    return (
-      <div>
-        <Tab.Container id="left-tabs-example" activeKey={key}>
-          <Row>
-            <Col sm={12}>
-              <Nav variant="pills" style={{ cursor: 'default' }}>
-                {tabs.map(({ id, title }) => (
-                  <Nav.Item>
-                    <Nav.Link eventKey={id} onClick={() => setKey(id)}>
-                      {title}
-                      <span
-                        onClick={(e) => handleRemoveTab(id)}
-                        style={{ marginLeft: 10, cursor: 'pointer' }}
-                      >
-                        ❌
-                      </span>
-                    </Nav.Link>
-                  </Nav.Item>
-                ))}
-                <Nav.Item onClick={handleAddTab}>
-                  <Nav.Link style={{ cursor: 'pointer' }}>+</Nav.Link>
-                </Nav.Item>
-              </Nav>
-              <Tab.Content>
-                {tabs.map(({ id }) => (
-                  <Tab.Pane eventKey={id}>{children}</Tab.Pane>
-                ))}
-              </Tab.Content>
-            </Col>
-          </Row>
-        </Tab.Container>
-      </div>
-    );
-  }
-
   return (
-    <Tabs>
-      <TabList>
-        {tabs.map(({ title, id }) => (
-          <Tab key={id}>
-            {title}
-            <Button onClick={() => handleRemoveTab(id)} variant="outline-dark">
-              ✖
-            </Button>
-          </Tab>
-        ))}
-        <Button size="sm" onClick={handleAddTab} variant="outline-primary">
-          +
-        </Button>
-      </TabList>
-      {tabs.map(({ id }) => (
-        <TabPanel key={id}>{children}</TabPanel>
-      ))}
-    </Tabs>
+    <div>
+      <Tab.Container id="left-tabs-example" activeKey={key}>
+        <Row>
+          <Col sm={12}>
+            <Nav variant="pills" style={{ cursor: 'default' }}>
+              {tabs.map(({ id, title }) => (
+                <Nav.Item>
+                  <Nav.Link eventKey={id} onClick={() => setKey(id)}>
+                    {title}
+                    <span
+                      onClick={(e) => handleRemoveTab(id)}
+                      style={{ marginLeft: 10, cursor: 'pointer' }}
+                    >
+                      ❌
+                    </span>
+                  </Nav.Link>
+                </Nav.Item>
+              ))}
+              <Nav.Item onClick={handleAddTab}>
+                <Nav.Link style={{ cursor: 'pointer' }}>+</Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Tab.Content>
+              {tabs.map(({ id }) => (
+                <Tab.Pane eventKey={id}>{children}</Tab.Pane>
+              ))}
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </div>
   );
 };
 
